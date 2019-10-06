6d573aab7589e6e3316b2a5ec219c33fdcaa3dbd
Hassam mustafa <hassamduet@gmail.com>
2019-10-06 13:04:56 +0500

    new files

---
+++ t2.js
@@ -0,0 +1,11 @@
+
+ var userInput = prompt("Enter your city Name!");
+
+ if (userInput === "Karachi") {
+   document.write("Welcome to city of Light : " + userInput);
+ } else if (userInput === "KARACHI") {
+   document.write("Welcome to city of Light : " + userInput);
+ } else if (userInput === "karachi") {
+   document.write("Welcome to city of Light : " + userInput);
+} else {alert("Please Enter 'KARACHI' OR 'Karachi' OR 'karachi' ");
+ }
